04e49fb0cb6ea9d5bb8d2ee4464685ff22c07eaf
pepe <hola@hola.com>
2023-05-12 00:24:48 -0500

    test of ir

--- src/PIR_MQTT_esp32.ino
+++ src/PIR_MQTT_esp32.ino
@@ -2,6 +2,7 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include <Servo.h>
+#include <IRremote.h>
 
 /* ----- Third Libraries ----- */
 #include "config.h"  // Set your network SSID and password in this file
@@ -12,6 +13,7 @@
 /* ----- Ports ----- */
 #define LINEAR_HALL_SENSOR 34               // Pin for PIR sensor in the esp32 board
 #define SERVO_PIN 26 // ESP32 pin GIOP26 connected to servo motor
+#define IR_PIN 21   //IR Receiver Pin 3
 
 /* ----- Variables ----- */
 long mov_timer = 0;
@@ -22,6 +24,11 @@ char msg[50];
 int sensorValue;
 int previousSensorValue = 1901;
 
+decode_results results;
+
+// Servo servoMotor;
+IRrecv irrecv(IR_PIN);
+
 /* Topics */
 char topicSensor[] = "sensor";
 char topicMotor[] = "motor";
@@ -30,6 +37,8 @@ char topicMotor[] = "motor";
 
 // setup
 void setup() {
+  irrecv.enableIRIn();
+
   // set PIN_PIR a pin as an input
   pinMode(LINEAR_HALL_SENSOR, INPUT);
   servoMotor.attach(SERVO_PIN);  // attaches the servo on ESP32 pin
@@ -43,19 +52,27 @@ void setup() {
 
 // loop
 void loop() {
-  HandleMqtt();
-  long now = millis(); 
-  //get sensor data each 500ms 
-  if (now - mov_timer > 500) { 
-    mov_timer = now;
-    int sensorValue = analogRead(LINEAR_HALL_SENSOR);
-    Serial.println(sensorValue);
-    if(sensorValue < 1900 && previousSensorValue > 1900){
-      snprintf (msg, 50, "1");
-      PublisMqttString(topicSensor, msg);
-    }
-    previousSensorValue = sensorValue;
+
+  if (irrecv.decode(&results)){
+    long int decCode = results.value;
+    Serial.println(results.value);
+    irrecv.resume();
+    delay(10);
   }
+
+  // HandleMqtt();
+  // long now = millis(); 
+  // //get sensor data each 500ms 
+  // if (now - mov_timer > 500) { 
+  //   mov_timer = now;
+  //   int sensorValue = analogRead(LINEAR_HALL_SENSOR);
+  //   Serial.println(sensorValue);
+  //   if(sensorValue < 1900 && previousSensorValue > 1900){
+  //     snprintf (msg, 50, "1");
+  //     PublisMqttString(topicSensor, msg);
+  //   }
+  //   previousSensorValue = sensorValue;
+  // }
 
   // Sending Keep Alive message each 5 seconds
   // if (now - ka_timer > 5000) { 
